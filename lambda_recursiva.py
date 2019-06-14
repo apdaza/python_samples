624ac5641af9b037052d7d98b8fe18a0dd20540d
apdaza <apdaza@gmail.com>
2019-06-13 21:10:14 -0500

    ejemplo lambda recursivo

--- lambda_recursiva.py
+++ lambda_recursiva.py
@@ -5,3 +5,7 @@ add = Z(lambda f: lambda a, b: b if a <= 0 else 1 + f(a - 1, b))
 
 print add(1, 1)
 print add(8, 2)
+
+factorial = lambda x: 1 if x == 1 else x * factorial (x-1)
+
+print factorial(3)
