e98d3448bf6fa698acf2404887cbb74002691696
apdaza <apdaza@gmail.com>
2019-10-23 20:18:24 -0500

    versión 2 de ventiuna

--- ventiuna_v1/principal.py
+++ ventiuna_v1/principal.py
@@ -3,4 +3,5 @@ from ventiuna import *
 mazo = [(v,p) for v in ['A','2','3','4','5','6','7','8','9','J','Q','K'] for p in ['C','D','T','P']]
 
 mano = asignar_mano(mazo, 3)
+
 print(mano, sumar_mano(mano))
