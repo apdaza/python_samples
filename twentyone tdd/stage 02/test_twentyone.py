159e38fd263536372910c717d7113cab373eb11d
apdaza <apdaza@gmail.com>
2020-01-13 09:32:02 -0500

    agregados los pytest del 21

--- twentyone tdd/stage 02/test_twentyone.py
+++ twentyone tdd/stage 02/test_twentyone.py
@@ -6,3 +6,21 @@ dealer = Dealer()
 def test_new_round():
     dealer.new_round()
     assert len(dealer.hand) == 2
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 21 (True)
+def test_get_hand_total():
+    dealer.hand = ['A','J']
+    assert dealer.get_hand_total() == 21
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 18 (True)
+def test_get_hand_total_noases():
+    dealer.hand = ['3','Q','5']
+    assert dealer.get_hand_total() == 18
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 12 (True)
+def test_get_hand_total_2ases():
+    dealer.hand = ['A','J','A']
+    assert dealer.get_hand_total() == 12
--- twentyone tdd/stage 04/test_twentyone.py
+++ twentyone tdd/stage 04/test_twentyone.py
@@ -6,3 +6,32 @@ dealer = Dealer()
 def test_new_round():
     dealer.new_round()
     assert len(dealer.hand) == 2
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 21 (True)
+def test_get_hand_total():
+    dealer.hand = ['A','J']
+    assert dealer.get_hand_total() == 21
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 18 (True)
+def test_get_hand_total_noases():
+    dealer.hand = ['3','Q','5']
+    assert dealer.get_hand_total() == 18
+
+# Prueba para verificar el valor total de una mano
+# Resultado: 12 (True)
+def test_get_hand_total_2ases():
+    dealer.hand = ['A','J','A']
+    assert dealer.get_hand_total() == 12
+
+# Prueba para verificar la funcion de jugar
+# Resultado: hit (True)
+def test_determine_play():
+    assert dealer.determine_play(15) == 'hit'
+
+# Prueba para verificar la funcion de hacer jugada
+# Resultado: stand (True)
+def test_make_play():
+    dealer.hand = ['3','Q','5']
+    assert dealer.make_play() == 'stand'
